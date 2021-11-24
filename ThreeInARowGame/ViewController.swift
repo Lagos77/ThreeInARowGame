672be202f6a17b9ba2afe745e4115a2fd61fac0e
Lagos77 <social_a@live.com>
2021-11-24 17:27:17 -0500

    Added "X" & "O" function

--- ThreeInARowGame/ViewController.swift
+++ ThreeInARowGame/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var tappedCircle : Bool = true
+    private var tappedCross : Bool = false
+    private var currentPlayer = ""
+    
     @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var btnOne: UIButton!
     @IBOutlet weak var btnTwo: UIButton!
@@ -20,19 +24,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnEight: UIButton!
     @IBOutlet weak var btnNine: UIButton!
     
+    var crosses = "X"
+    var circles = "O"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //Initilaizing cross as first move
+        //var firstTurn = tappedCross
+        //var currentTurn = tappedCross
+        
+
+        
     }
     
     @IBAction func btnTapped(_ sender: UIButton) {
-        print("button pressed!")
+        gameManager(sender)
         
+    }
+    
+    func gameManager(_ sender: UIButton){
+        _ = tappedCross
+        _ = tappedCross
+        
+        if currentPlayer == "X" {
+            sender.setTitle("X", for: .normal)
+            currentPlayer = "O"
+        } else {
+            sender.setTitle("O", for: .normal)
+            currentPlayer = "X"
+        }
+        
+
     }
     
 
     
 }
 
+/*if(sender.title(for: .normal) == nil){
+    if(currentTurn == tappedCircle){
+        sender.setTitle(crosses, for: .normal)
+        currentTurn = tappedCross
+        infoLabel.text = crosses
+    } else {
+        if(currentTurn == tappedCross){
+            sender.setTitle(circles, for: .normal)
+            currentTurn = tappedCircle
+            infoLabel.text = circles
+        }
+     }
+}
+*/
