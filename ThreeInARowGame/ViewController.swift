12f8ec1350765578adcd3127a41fe21402a519b1
Lagos77 <social_a@live.com>
2021-11-24 16:14:56 -0500

    Added buttons with actions. Hard to find information with image view + tap recognizer

--- ThreeInARowGame/ViewController.swift
+++ ThreeInARowGame/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var btnOne: UIButton!
+    @IBOutlet weak var btnTwo: UIButton!
+    @IBOutlet weak var btnThree: UIButton!
+    @IBOutlet weak var btnFour: UIButton!
+    @IBOutlet weak var btnFive: UIButton!
+    @IBOutlet weak var btnSix: UIButton!
+    @IBOutlet weak var btnSeven: UIButton!
+    @IBOutlet weak var btnEight: UIButton!
+    @IBOutlet weak var btnNine: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func btnTapped(_ sender: UIButton) {
+        print("button pressed!")
+        
+    }
+    
 
-
+    
 }
 
