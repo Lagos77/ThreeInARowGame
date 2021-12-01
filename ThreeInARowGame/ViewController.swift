c6a24b46826ac69705dedaffc612a50f6a4f1fbe
Lagos77 <social_a@live.com>
2021-12-01 17:28:30 -0500

    Entering game rules

--- ThreeInARowGame/ViewController.swift
+++ ThreeInARowGame/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var tappedCircle : Bool = true
-    private var tappedCross : Bool = false
-    private var currentPlayer = ""
+    private var buttons = [UIButton]()
+    private var currentPlayer = 0
+    var board = [String]()
     
-    @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var btnOne: UIButton!
     @IBOutlet weak var btnTwo: UIButton!
     @IBOutlet weak var btnThree: UIButton!
@@ -24,58 +23,122 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnEight: UIButton!
     @IBOutlet weak var btnNine: UIButton!
     
-    var crosses = "X"
-    var circles = "O"
+    @IBOutlet weak var infoLabel: UILabel!
+    var playerCross = 0
+    var playerCircle = 0
+
+    
+    var playerX = [Int]()
+    var playerO = [Int]()
+    
+    //Set scores to 0 for both player
+    
+    
+    //Array list of UIButton    var playerX = [UIButton]()
+    
+    //var crosses = "X"
+    //var circles = "O"
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        buttons.append(btnOne)
+        buttons.append(btnTwo)
+        buttons.append(btnThree)
+        buttons.append(btnFour)
+        buttons.append(btnFive)
+        buttons.append(btnSix)
+        buttons.append(btnSeven)
+        buttons.append(btnEight)
+        buttons.append(btnNine)
         
+        boardGame()
+    
         //Initilaizing cross as first move
         //var firstTurn = tappedCross
         //var currentTurn = tappedCross
+        //playerNameAlert()
         
-
         
     }
     
     @IBAction func btnTapped(_ sender: UIButton) {
+        let index = buttons.firstIndex(of: sender)!
+        print(index)
         gameManager(sender)
+    
         
     }
     
     func gameManager(_ sender: UIButton){
-        _ = tappedCross
-        _ = tappedCross
         
-        if currentPlayer == "X" {
+        if currentPlayer == 0 {
             sender.setTitle("X", for: .normal)
-            currentPlayer = "O"
+            currentPlayer = 1
             infoLabel.text = "Player O turn"
         } else {
             sender.setTitle("O", for: .normal)
-            currentPlayer = "X"
+            currentPlayer = 0
             infoLabel.text = "Player X turn"
         }
+        //Set button to be clicked only once
+        sender.isEnabled = false
         
 
     }
-    
-
-    
-}
-
-/*if(sender.title(for: .normal) == nil){
-    if(currentTurn == tappedCircle){
-        sender.setTitle(crosses, for: .normal)
-        currentTurn = tappedCross
-        infoLabel.text = crosses
-    } else {
-        if(currentTurn == tappedCross){
-            sender.setTitle(circles, for: .normal)
-            currentTurn = tappedCircle
-            infoLabel.text = circles
+    //Recives a string and returns a bool
+    //Check win possibilities if symbol on button title matches with each other
+    func checkWin(_ string: String) -> Bool {
+        
+        //Horizontical possibilites
+        if thisSymbol(btnOne, string) && thisSymbol(btnTwo, string) && thisSymbol(btnThree, string){
+            return true
         }
-     }
+        if thisSymbol(btnFour, string) && thisSymbol(btnFive, string) && thisSymbol(btnSix, string){
+            return true
+        }
+        if thisSymbol(btnSeven, string) && thisSymbol(btnEight, string) && thisSymbol(btnNine, string){
+            return true
+        }
+        
+        //Diagonal possibilities
+        if thisSymbol(btnOne, string) && thisSymbol(btnFive, string) && thisSymbol(btnNine, string){
+            return true
+        }
+        if thisSymbol(btnSeven, string) && thisSymbol(btnFive, string) && thisSymbol(btnThree, string){
+            return true
+        }
+        
+        return false
+    }
+    
+    //Recives a UIButton and a string and return a bool
+    //If the button title has a X or O, if so return true
+    func thisSymbol(_ button : UIButton,_ symbol: String) -> Bool {
+        return button.title(for: .normal) == symbol
+    }
+    
+    func boardGame(){
+        for i in 0..<buttons.count{
+            board.append("")
+            
+        }
+    
+    }
+    
+    
+    //Function doesnt work properly. Suppose to show at up-start
+    /*func playerNameAlert(){
+        
+        
+        let alert = UIAlertController(title: "Enter player one name", message: nil, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in }))
+        
+        
+        
+    }
+    */
 }
-*/
